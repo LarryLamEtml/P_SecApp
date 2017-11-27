4c863c5ef79b96fd1db5661a41ab5d722a3a8676
Unknown <justin_vuffray@hotmail.com>
2017-11-27 11:01:24 +0100

    sd

--- Terminal/bruteForce/bruteForce/Program.cs
+++ Terminal/bruteForce/bruteForce/Program.cs
@@ -7,9 +7,9 @@ namespace bruteForce
         static void Main(string[] args)
         {
             String password;
-            
-            BruteForceHTTP btForce = new BruteForceHTTP("http://127.0.0.1:8080/edsa-shop/index.php?controller=login&action=login", "login", "password", @"C:\Users\vuffrayju\Downloads\test.txt");
             DateTime dt = DateTime.Now;
+            BruteForceHTTP btForce = new BruteForceHTTP("http://127.0.0.1:8080/edsa-shop/index.php?controller=login&action=login", "login", "password", @"C:\Users\vuffrayju\Downloads\test.txt");
+            
             password = btForce.findPassword();
             DateTime dt2 = DateTime.Now;
             Console.Write(password+"\n"+(dt2-dt).TotalSeconds+"s");
